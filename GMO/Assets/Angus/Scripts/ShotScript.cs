ef8314c574914ed07536279179c21e432d85e656
Angus Cheng <baller.industries@hotmail.com>
2015-01-24 20:20:23 +0800

    Angus's version of this pixelnest tutorial: http://pixelnest.io/tutorials/2d-game-unity/table-of-contents/

---
+++ GMO/Assets/Angus/Scripts/ShotScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Angus
+{
+    public class ShotScript : MonoBehaviour
+    {
+        public int damage;
+        public bool isEnemyShot;
+
+        // Use this for initialization
+        void Start()
+        {
+            Destroy(gameObject, 20);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
